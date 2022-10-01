859049d9c737318fbd8a05dd30aa2d538b2e50b1
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-01 14:49:50 -0400

    minor change to seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO employees(first_name,last_name)
 VALUES
 -- RANDOM VALUES TO POPULATE DATABASE
-("John", "John"),
-("Mike", "Mike"),
-("Sarah", "Sarah"),
-("Daphni","Daphni")
+("John", "Daphni"),
+("Mike", "John"),
+("Sarah", "Mike"),
+("Daphni","Sarah")
