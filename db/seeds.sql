2dcc5b675f9662a0c47f703abeee08d383456610
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-01 16:49:49 -0400

    updated sql for utils functions to be made now

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,19 @@
-INSERT INTO employees(first_name,last_name)
+INSERT INTO dept (deptartment_name)
+VALUES
+("Sales"),
+("Customer Service"),
+("Executive Management");
+
+INSERT INTO emp_role (role_title)
+VALUES
+("Sales Representative"),
+("Customer Service Representative"),
+("CEO");
+
+INSERT INTO employees (first_name,last_name)
 VALUES
 -- RANDOM VALUES TO POPULATE DATABASE
 ("John", "Daphni"),
 ("Mike", "John"),
 ("Sarah", "Mike"),
-("Daphni","Sarah")
+("Daphni","Sarah");
